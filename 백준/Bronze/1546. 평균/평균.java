931213e73566aa76a154cd864088a70cfd312f58
jiyun-im-dev <133330951+jiyun-im-dev@users.noreply.github.com>
2024-10-17 16:27:27 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18920 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,23 +1,20 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		float[] scores = new float[n];
-		for (int i = 0; i < n; i++) {
-			scores[i] = sc.nextLong();
-		}
-		Arrays.sort(scores); // 오름차순 정렬
-		float max = scores[n - 1];
-		float sum = 0;
-		// 계산한 값을 배열에 다시 넣는다
-		for (int i = 0; i < n; i++) {
-			scores[i] = scores[i] / max * 100;
-			sum += scores[i];
-		}
-		float avg = sum / n;
-		System.out.println(avg);
-	}
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int max = 0;
+        int tmp;
+        float avg;
+        for (int i = 0; i < n; i++) {
+            tmp = sc.nextInt();
+            sum += tmp;
+            if (tmp > max) max = tmp;
+        }
+        avg = (float)sum / n / max * 100;
+        System.out.println(avg);
+    }
 }
