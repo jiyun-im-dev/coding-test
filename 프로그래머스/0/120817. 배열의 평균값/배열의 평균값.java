0cc729ba4f47f13c796877aeb79cc3e027b8601b
jiyun-im-dev <133330951+jiyun-im-dev@users.noreply.github.com>
2025-03-05 09:16:46 +0900

    [level 0] Title: 배열의 평균값, Time: 3.39 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public double solution(int[] numbers) {
-        double answer = Arrays.stream(numbers).reduce(0, Integer::sum);
-        return answer / numbers.length;
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
